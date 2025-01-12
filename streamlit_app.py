370e5c19f3ad3ea2c83e7cd7a1a1c4179201ad8f
moatboy <tedekaam2@gmail.com>
2025-01-12 13:52:18 +0000

    format

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,21 @@ import pandas as pd
 import streamlit as st
 import yfinance as yf
 
+def format_large_number(number):
+    if number >= 1_000_000_000_000:
+        return f"{number / 1_000_000_000_000:.1f}T"  # Trillions
+    elif number >= 1_000_000_000:
+        return f"{number / 1_000_000_000:.1f}B"  # Billions
+    elif number >= 1_000_000:
+        return f"{number / 1_000_000:.1f}M"  # Millions
+    elif number >= 1_000:
+        return f"{number / 1_000:.1f}K"  # Thousands
+    else:
+        return str(number)  # No formatting needed
+
 def get_market_cap(ticker):
     stock = yf.Ticker(ticker)
-    return stock.info['marketCap'] if 'marketCap' in stock.info else None
+    return format_large_number(stock.info['marketCap']) if 'marketCap' in stock.info else None
 
 # Show the page title and description.
 st.set_page_config(page_title="MoatBoy Screener", page_icon="📈")
@@ -36,7 +48,7 @@ moat = st.slider("Moat", 1, 5, (1, 5))
 df_filtered = df[(df["moat"].between(moat[0], moat[1])) & (df["management"].between(management[0], management[1]))\
                  & (df["catalyst"].between(catalyst[0], catalyst[1]))]
 
-df_filtered['marketcap'] = df['ticker'].apply(get_market_cap)
+df_filtered['marketcap (in millions)'] = df['ticker'].apply(get_market_cap)
 
 # Display the data as a table using `st.dataframe`.
 st.dataframe(
